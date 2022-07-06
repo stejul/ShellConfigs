24cd7ae58bfe86cb895399bb47b4082660b95f39
stejul <stejul@rocketmail.com>
2022-07-06 17:26:52 +0200

    feat: added fira code nerd font

--- wezterm.lua
+++ wezterm.lua
@@ -5,4 +5,5 @@ return {
   default_prog = { "/bin/zsh", "-l", "-c", "tmux" },
   initial_cols = 200,
   initial_rows = 50,
+  font = wezterm.font("FiraCode Nerd Font Mono", { weight = "Regular"}),
 }
