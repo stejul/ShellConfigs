bbcd09782425b633c94a7c89513583b7bf84ec04
stejul <stejul@rocketmail.com>
2022-07-06 16:32:34 +0200

    fix: don't use symlinks

--- wezterm.lua
+++ wezterm.lua
@@ -1,1 +1,8 @@
-../wezterm/wezterm.lua
+local wezterm = require "wezterm";
+
+return {
+  color_scheme = "tokyonight",
+  default_prog = { "/bin/zsh", "-l", "-c", "tmux" },
+  initial_cols = 200,
+  initial_rows = 50,
+}
